acea8454a7d8683b4566b1370b7e770da3231b90
EC2 Default User <yasuzyun@yahoo.co.jp>
2023-02-24 01:27:19 +0000

    Initial Commit

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,6 +1,5 @@
 $(function(){
-  $('.box1').css({
-    'background-color': '#0000FF',
-    'height': '100px'
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000FF'});
   });
 });
